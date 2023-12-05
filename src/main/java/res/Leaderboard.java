771eb524fa8da3ca3e45d5d24f2fd9e9c7cc2c89
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-12-05 21:07:03 +0800

    Fixed leaderboard delete

--- src/main/java/res/Leaderboard.java
+++ src/main/java/res/Leaderboard.java
@@ -12,114 +12,112 @@ import java.util.*;
  */
 public class Leaderboard {
     private double min;
-        private int length;
-        private Map<String, Player> playerMap;
-        private Player[] ranking;
+    private int length;
+    private Map<String, Player> playerMap;
+    private Player[] ranking;
 
-        public Leaderboard() {
-            min = 0;
-            length = 0;
-            playerMap = new HashMap<>();
-            ranking = new Player[20];
-        }
+    public Leaderboard() {
+        min = 0;
+        length = 0;
+        playerMap = new HashMap<>();
+        ranking = new Player[20];
+    }
 
-        public void checkInsert(Player p) {
-            int startIndex;
-            if (playerMap.containsKey(p.name)) {
-                Player old_p = playerMap.get(p.name);
+    public void checkInsert(Player p) {
+        int startIndex;
+        if (playerMap.containsKey(p.name)) {
+            Player old_p = playerMap.get(p.name);
 
-                if (old_p.score >= p.score)
-                    return; // old still beats
+            if (old_p.score >= p.score)
+                return; // old still beats
 
-                old_p.score = p.score;
-                startIndex = findIndex(p);
-            } else {
-                // check if new player fits in ranking
-                if (length < 20) {
-                    length++;
-                    
+            old_p.score = p.score;
+            startIndex = findIndex(p.name);
+        } else {
+            // check if new player fits in ranking
+            if (length < 20) {
+                length++;
+
                 // not fit for leaderboard
-                } else if (p.score <= min) {
-                    return; // no point in editing
-                
+            } else if (p.score <= min) {
+                return; // no point in editing
+
                 // it's better than last player so we good
-                } else {
-                    playerMap.remove(ranking[length - 1].name);
-                }
-                    
+            } else {
+                playerMap.remove(ranking[length - 1].name);
+            }
 
-                ranking[length - 1] = p;
-                playerMap.put(p.name, p);
-                startIndex = length - 1;
-            }
-            if (length != 1)
-                sortUp(startIndex);
-            // since we sorted, the last element would
-            // always have the lowest score
-            min = ranking[length - 1].score;
+            ranking[length - 1] = p;
+            playerMap.put(p.name, p);
+            startIndex = length - 1;
         }
-        
-        public Player[] toArray() {
-            Player[] out_list = new Player[length];
-            for (int i = 0; i < length; i++) {
-                out_list[i] = ranking[i];
-            }
-            return out_list;
+        if (length != 1)
+            sortUp(startIndex);
+        // since we sorted, the last element would
+        // always have the lowest score
+        min = ranking[length - 1].score;
+    }
+
+    public Player[] toArray() {
+        Player[] out_list = new Player[length];
+        for (int i = 0; i < length; i++) {
+            out_list[i] = ranking[i];
         }
-        
-        public boolean deletePlayer(String name) {
-            if (!playerMap.containsKey(name)) {
-                return false;
-            }
-            
-            // deletes a player starting from the top of the array
-            int i = ranking.length-1;
-            Player temp = ranking[i];
-            while (i > 0) {
-                if (!ranking[i-1].name.equals(name)) {
-                    ranking[i-1] = temp;
-                    temp = ranking[i-1];
-                } else {
-                    break;
-                }
-                
-                i--;
-            }
-            length--;
-            playerMap.remove(name);
-            return true;
+        return out_list;
+    }
+
+    // Does not really delete, mostly dereferences the player from the leaderboard
+    // array
+    public boolean deletePlayer(String name) {
+        if (!playerMap.containsKey(name)) {
+            return false;
         }
 
-        // the insertion part in insertion sort
-        // runs fairly fast, at worst 20 times
-        // we just jog up the array finding things to move
-        private void sortUp(int start) {
-            Player key = ranking[start];
-            int i = start-1;
-            while (i>=0 && ranking[i].score < key.score) {
-                ranking[i+1] = ranking[i];
-                i--;
-            }
-            ranking[i+1] = key;
+        // replaces the player using the items from the top
+        int i = findIndex(name);
+
+        while (i < length - 1) {
+            ranking[i] = ranking[i + 1];
+            i++;
         }
 
-        // binary search ;;;;
-        private int findIndex(Player p) {
-            int l = 0;
-            int r = length - 1;
-            while (l <= r) {
-                int m = l + (r - 1) / 2;
+        // then we update the leaderboard to not include the dereferenced player
+        length--;
+        playerMap.remove(name);
+        return true;
+    }
 
-                if (ranking[m] == p)
-                    return m;
-
-                if (ranking[m].score < p.score)
-                    l = m + 1;
-                else
-                    r = m - 1;
-            }
-            return 0; // would be zero if the item is yet to be added
+    // the insertion part in insertion sort
+    // runs fairly fast, at worst 20 times
+    // we just jog up the array finding things to move
+    private void sortUp(int start) {
+        Player key = ranking[start];
+        int i = start - 1;
+        while (i >= 0 && ranking[i].score < key.score) {
+            ranking[i + 1] = ranking[i];
+            i--;
         }
+        ranking[i + 1] = key;
+    }
 
-        
+    // binary search ;;;;
+    private int findIndex(String playerName) {
+        int l = 0;
+        int r = length - 1;
+        Player p = playerMap.get(playerName);
+
+        while (l <= r) {
+            int m = l + (r - 1) / 2;
+
+            if (ranking[m] == p)
+                return m;
+
+            if (ranking[m].score < p.score)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        return 0; // would be zero if the item is yet to be added
+    }
+
 }
